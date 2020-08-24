359f26278ffebf985e95a81f958e0ab5c54134aa
Nicolas Demol <nyco.demol@gmail.com>
2020-08-25 00:36:46 +0200

    change for vuetify

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar app color="white" shrink-on-scroll float>
+      <v-toolbar-title class="nav-title mx-auto">D&C</v-toolbar-title>
+    </v-app-bar>
+    <v-sheet class="overflow-y-auto">
+      <v-container style="height: 2000px;"> </v-container>
+    </v-sheet>
+  </v-card>
+</template>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Jacques+Francois+Shadow&display=swap");
+
+.nav-title {
+  font-family: "Jacques Francois Shadow", cursive;
+}
+</style>
