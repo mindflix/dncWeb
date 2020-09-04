a27d431dd1c93d57b614b4b9dc8105e966e3fa7f
Nicolas Demol <nyco.demol@gmail.com>
2020-09-04 02:32:21 +0200

    test image

---
+++ src/components/Categories.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-img :src="image"></v-img>
+</template>
+
+<script>
+import image from "../assets/voirplus.png";
+
+export default {
+  name: "Categories",
+  data: function () {
+    return {
+      image: image,
+    };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="home">
     <v-sheet class="overflow-y-auto">
-      <v-container style="height: 2000px;"></v-container>
+      <v-container style="height: 2000px;">
+        <Categories />
+      </v-container>
     </v-sheet>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Categories from "../components/Categories";
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Categories,
+  },
 };
 </script>
